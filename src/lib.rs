a8e34fd79b9f165620f1cb5a0e22d664a8941582
H3r3zy <sahel.lucas-saoudi@epitech.eu>
2024-05-13 17:02:04 +0200

    update neon + fastcdc with v2020

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,25 @@
 use neon::prelude::*;
-use fastcdc::*;
+use fastcdc::v2020::*;
+use neon::types::buffer::TypedArray;
 
 fn get_chunks (mut cx: FunctionContext) -> JsResult<JsArray> {
     // unbox and check arguments
     let bytes_handle = cx.argument::<JsArrayBuffer>(0)?;
-    let min_size = cx.argument::<JsNumber>(1)?.value(&mut cx) as usize;
-    let avg_size = cx.argument::<JsNumber>(2)?.value(&mut cx) as usize;
-    let max_size = cx.argument::<JsNumber>(3)?.value(&mut cx) as usize;
+    let min_size = cx.argument::<JsNumber>(1)?.value(&mut cx) as u32;
+    let avg_size = cx.argument::<JsNumber>(2)?.value(&mut cx) as u32;
+    let max_size = cx.argument::<JsNumber>(3)?.value(&mut cx) as u32;
 
     // read all of the cuts into a temporary vector
+    let bytes = bytes_handle.as_slice(&cx);
     let mut cuts = Vec::new();
-    cx.borrow(&bytes_handle, |bytes_ref| {
-        let bytes = bytes_ref.as_slice::<u8>();
-        let chunker = FastCDC::new(&bytes, min_size, avg_size, max_size);
-        for entry in chunker {
-            cuts.push(entry.offset);
-        }
-        cuts.push(bytes.len())
-    });
-    
+    let chunker = FastCDC::new(&bytes, min_size, avg_size, max_size);
+    for entry in chunker {
+        cuts.push(entry.offset);
+    }
+    cuts.push(bytes.len());
+
     // copy the cuts into a js array
-    let result = JsArray::new(&mut cx, cuts.len() as u32);
+    let result = JsArray::new(&mut cx, cuts.len());
     for (i, v) in cuts.iter().enumerate() {
         let n = cx.number(*v as f64);
         result.set(&mut cx, i as u32, n)?;
